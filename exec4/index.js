fb5b1186ef973cd35c0c06e0e01f55ce3e846ea8
Hamilton Junior <hamilton_jrribeiro@hotmail.com>
2022-06-21 19:03:36 -0300

    Adicionado dois exercicios solucionados

---
+++ exec4/index.js
@@ -0,0 +1,16 @@
+/* PRESSUPOSTO DO EXERCÍCIO */
+for (let i = 0; i <= 10; i++) {
+    console.log(`O número atual é: ${i}`)
+} 
+
+/* 
+for (let i = 0; i <= 10; i++) {
+    if (i === 0) {
+        console.log(`The first number is: ${i}`)
+    } else if (i === 10) {
+        console.log(`The last number is: ${i}`)
+    } else {
+        console.log(`The current number: ${i}`)
+    }
+} 
+*/
---
+++ exec5/index.js
@@ -0,0 +1,3 @@
+for (i = 0; i <= 10; i ++) {
+    console.log(`O dobro do número ${i} é ${i * 2}`)
+}
